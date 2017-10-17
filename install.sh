79b3b6d8472040fb94b4486c35ec12b13988aa2d
matisla <herrmann.matthieu@gmail.com>
2017-10-18 00:24:46 +0200

    add installation script and zshrc file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# INSTALLATION 
+
+# ZSH
+
+ln zsh/zshrc $HOME/.zshrc
+ln zsh/matisla.zsh-theme $HOME/.oh-my-zsh/theme/matisla.zsh-theme
+
+# VIM
+
+ln vim/vimrc $HOME/.vimrc
+ln vim/mappings.vim $HOME/.vim/mappings.vim
